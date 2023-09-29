071825b204d7b9c8fa11fce28f5fd41d3b32a9f5
Israel Batista <israel.batista031@gmail.com>
2023-09-28 22:16:42 -0300

    Capítulo 2, Aula 05 - Lidando com Request e Response

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $series = array(
             'Punisher',
@@ -19,6 +19,6 @@ class SeriesController extends Controller
             $html .= "<li>{$serie}</li>";
         }
 
-        echo $html;
+        return $html;
     }
 }
