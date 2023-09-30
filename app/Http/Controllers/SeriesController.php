29002ffc0fae8c4804b93fde9f5af5c0d60839a4
Israel Batista <israel.batista031@gmail.com>
2023-09-30 13:30:35 -0300

    Capítulo 5, Aula 06 - DB Facade

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,17 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        $series = array(
-            'Punisher',
-            'Lost',
-            'Grey\'s Anatomy',
-        );
-
+        $series = DB::select('SELECT nome FROM series');
 
         return view('series.index')->with('series', $series);
     }
@@ -21,5 +17,16 @@ class SeriesController extends Controller
     public function create()
     {
         return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $nomeSerie = $request->input('nome');
+        
+        if (DB::insert('INSERT INTO series (nome) VALUES (?)', [$nomeSerie])) {
+            return "OK";
+        } else {
+            return "deu erro";
+        }
     }
 }
