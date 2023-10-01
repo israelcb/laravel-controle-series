058e51f5df48361ccc62dd520a1f108ce28be420
Israel Batista <israel.batista031@gmail.com>
2023-10-01 17:55:04 -0300

    Curso 3, Capítulo 3, Aula 02 - Listando episódios

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\EpisodesController;
 use App\Http\Controllers\SeasonsController;
 use App\Http\Controllers\SeriesController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,3 +25,8 @@ Route::resource('/series', SeriesController::class)
     ->except('show');
 
 Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
+
+Route::get('/season/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
+Route::post('/season/{season}/episodes', function(Request $request) {
+    dd($request->all());
+});
