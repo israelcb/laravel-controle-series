ebe0c7cf68052b2a1ee8b50ae6035ceb7600922a
Israel Batista <israel.batista031@gmail.com>
2023-10-10 14:33:10 -0300

    Curso 3, Capítulo 5, Aula 05 - Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
-use App\Http\Controllers\EpisodesController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\SeasonsController;
-use App\Http\Controllers\SeriesController;
-use App\Http\Controllers\UsersController;
-use App\Http\Middleware\Authenticator;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,29 +9,23 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::resource('/series', SeriesController::class)
-    ->except('show');
-
-Route::middleware('autenticador')->group(function() {
-    Route::get('/', function () {
-        return redirect('/series');
-    });
-    
-    Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])
-        ->name('seasons.index');
-    
-    Route::get('/season/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
-    Route::post('/season/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'store'])->name('signin');
-Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/register', [UsersController::class, 'create'])->name('users.create');
-Route::post('/register', [UsersController::class, 'store'])->name('users.store');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
